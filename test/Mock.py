6fbd7a9334bbb025f9465b471cf4b1744f9d831e
2511258h <2511258h@student.gla.ac.uk>
2020-05-18 08:34:33 +0100

    Mock

---
+++ test/Mock.py
@@ -0,0 +1,20 @@
+import unittest
+import tweeter
+from mock import Mock
+
+
+
+class twiitTest(unittest.TestCase):
+    def test1(self):
+        Mock_tweet = Mock()
+        tweeter.twiit(Mock_tweet, "Test1")
+        Mock_tweet.PostUpdate.assert_called_with("msg")
+
+    def tes2(self):
+        Mock_tweet = Mock()
+        tweeter.twiit(Mock_tweet, "Test2")
+        Mock_tweet.PostUpdate.assert_called_with("msg")
+
+
+if __name__ == '__main__':
+    unittest.main()
